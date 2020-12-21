a5a591f1f1889e8af32b674eb6dc0de3d81c47e8
matago <matthew.agostinelli@gmail.com>
2020-12-21 02:11:10 -0500

    feat: day 01

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+mod days;
+use days::day01;
+
+#[derive(StructOpt, Debug)]
+enum Days {
+    Day01,
+    Day02,
+}
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(subcommand)]
+    day: Days,
+}
+#[tokio::main]
+async fn main() {
+    let opt = Opt::from_args();
+    match opt.day {
+        Days::Day01 => {
+            println!(
+                "Day 1, part 1: {}",
+                day01::run(day01::Part::One).await.unwrap()
+            );
+            println!(
+                "Day 1, part 2: {}",
+                day01::run(day01::Part::Two).await.unwrap()
+            );
+        }
+        Days::Day02 => println!("Still working on it..."),
+        _ => println!("Not implemented"),
+    }
 }
